a8957f7f97f0974dc65f784bd01bf824185a9a65
thang <100179094+Thangtran496@users.noreply.github.com>
2023-09-22 10:17:19 +0700

    add bootstrap

--- src/components/Trangchu.vue
+++ src/components/Trangchu.vue
@@ -34,7 +34,7 @@ export default {
         this.posts = response.data;
       })
       .catch((e) => {
-        this.errors.push(e);
+
       });
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
